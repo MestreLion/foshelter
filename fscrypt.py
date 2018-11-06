972c78d77a221ce2e0d36cbb714e1571facdbc42
MestreLion <linux@rodrigosilva.com>
2018-11-06 12:05:12 -0200

    add command line options and improve documentation

--- fscrypt.py
+++ fscrypt.py
@@ -17,54 +17,60 @@
 
 
 """
-    Fallout Shelter save game encryptor and decryptor
+Fallout Shelter save game encryptor and decryptor
 
-    Saved games are Vault*.sav files, located at:
+Saved games are Vault*.sav files, generally located at:
 
-    Android: /Android/data/com.bethsoft.falloutshelter/files
-    Windows: C:\\Users\\YOURUSERNAME\\Documents\\My Games\\Fallout Shelter
-    Steam:   C:\\Users\\YOURUSERNAME\\AppData\\Local\\FalloutShelter
+Android: /Android/data/com.bethsoft.falloutshelter/files
+Windows: C:\\Users\\<YOURUSERNAME>\\Documents\\My Games\\Fallout Shelter
+Steam:   C:\\Users\\<YOURUSERNAME>\\AppData\\Local\\FalloutShelter
 
-    Currently reads binary data from stdin and output formatted JSON to stdout
+Decrypts binary data read from stdin outputting formatted JSON   to stdout
+Encrypts JSON   data read from stdin outputting binary save data to stdout
 
-    Usage:
-        python3 fscrypt.py < Vault1.save > Vault1.json
+Examples:
+    python3 fscrypt.py    < Vault1.sav  > Vault1.json  # Decrypt by default
+    python3 fscrypt.py -e < Vault1.json > Vault1.sav
 
-    Constants taken from disassembled game source code:
-    https://androidrepublic.org/threads/6181
-
-    IV is used as both PBKDF2 key salt and AES IV.
-    Its value was very likely chosen copying from an old StackOverflow answer
-    https://stackoverflow.com/revisions/10177020/2
-
-    KEY is a precomputed AES key, stored here as a base16-encoded string (ASCII
-    hex format), and can be generated using the IV and a hardcoded password:
-    PASSWORD = base64.b64encode(b'PlayerData')[:8]  # or simply b'UGxheWVy'
-    See previous commits on how to manually generate it
+Constants taken from disassembled game source code:
+https://androidrepublic.org/threads/6181
 """
 
 
 import sys
 import base64
 import json
+import argparse
 
 import Crypto.Cipher.AES as AES  # PyPI: pip install pycrypto
 
 
+# IV is used as both PBKDF2 key salt and AES IV.
+# Its value was very likely chosen copying from an old StackOverflow answer:
+# https://stackoverflow.com/revisions/10177020/2
 IV  = b'tu89geji340t89u2'
+
+# KEY is a precomputed AES key, stored here as a base16-encoded string (ASCII
+# hex format), and can be generated using the IV and a hardcoded password:
+# PASSWORD = base64.b64encode(b'PlayerData')[:8]  # or simply b'UGxheWVy'
+# See previous commits on how to manually generate it
 KEY = b'A7CA9F3366D892C2F0BEF417341CA971B69AE9F7BACCCFFCF43C62D1D7D021F9'
+
 CIPHER = AES.new(base64.b16decode(KEY), AES.MODE_CBC, IV)
+
+COPYRIGHT="""
+Copyright (C) 2018 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
+License: GPLv3 or later, at your choice. See <http://www.gnu.org/licenses/gpl>
+"""
 
 
 def decrypt(savedata: str) -> dict:
-    '''
-    Decrypts a Fallout Shelter save game data to a Dictionary
-    '''
+    """Decrypt a Fallout Shelter save game data to a Dictionary."""
 
     # Decode and decrypt the save data
     data = CIPHER.decrypt(base64.b64decode(savedata.encode('ascii')))
 
-    # Remove trailing padding (N bytes of value N)
+    # Remove PKCS#7 trailing padding (N bytes of value N)
     data = data[:-data[-1]]
 
     # Deserialize JSON string to Python dict object
@@ -72,14 +78,12 @@ def decrypt(savedata: str) -> dict:
 
 
 def encrypt(obj: dict) -> str:
-    '''
-    Encrypt a Dictionary to a Fallout Shelter save game data
-    '''
+    """Encrypt a Dictionary to a Fallout Shelter save game data."""
 
     # Serialize to a one-line JSON byte string
     data = json.dumps(obj).encode('ascii')
 
-    # Add padding
+    # Add PKCS#7 padding
     pad = 16 - len(data) % 16
     data += pad * bytes((pad,))
 
@@ -88,12 +92,28 @@ def encrypt(obj: dict) -> str:
 
 
 def prettyjson(obj: dict) -> str:
-    return json.dumps(obj, sort_keys=True, indent=4, separators=(',',':'))
+    return json.dumps(obj, sort_keys=True, indent=4, separators=(',',':')) + '\n'
 
 
 
 
 if __name__ == '__main__':
-    # Print formatted output
-    #print(prettyjson(decrypt(sys.stdin.read())))
-    sys.stdout.write(encrypt(json.loads(sys.stdin.read())))
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        epilog=COPYRIGHT,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-d", "--decrypt", action="store_true", default=True,
+                        help="Decrypt save data to formatted JSON [Default]")
+    group.add_argument("-e", "--encrypt", dest='decrypt', action="store_false",
+                        help="Encrypt JSON to save data")
+    args = parser.parse_args()
+
+    data = sys.stdin.read()
+    if args.decrypt:
+        out = prettyjson(decrypt(data))
+    else:
+        out = encrypt(json.loads(data))
+
+    sys.stdout.write(out)
