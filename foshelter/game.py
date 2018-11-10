e3ef18486052727741453182d0e138e8854b9405
MestreLion <linux@rodrigosilva.com>
2018-11-10 19:06:42 -0200

    improve Game.from_save() error handling

--- foshelter/game.py
+++ foshelter/game.py
@@ -11,6 +11,8 @@ import json
 from . import orm
 from . import dwellers
 from . import savefile
+from . import util
+
 
 class Game(orm.Entity):
     """Root class for a game save"""
@@ -18,8 +20,21 @@ class Game(orm.Entity):
     @classmethod
     def from_save(cls, path, decrypted=False):
         with open(path, 'r') as fd:
-            data = json.load(fd) if decrypted else savefile.decrypt(fd.read())
-        return cls.from_data(data)
+            data = fd.read()
+
+        if decrypted:
+            try:
+                return cls.from_data(json.loads(data))
+            except json.decoder.JSONDecodeError as e:
+                raise util.FSException('Could not load Vault data,'
+                   ' is it a decrypted JSON file? %s: %s', path, e)
+
+        try:
+            return cls.from_data(savefile.decrypt(data))
+        except ValueError as e:
+            raise util.FSException('Could not load Vault data,'
+               ' is it an encrypted SAV file? %s: %s', path, e)
+
 
     def __init__(self, data: dict, **kw):
         super().__init__(data, **kw)
